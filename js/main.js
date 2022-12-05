7ad764f3017ba701ef667d14371e730894d02d74
Noahmarkovich <noahmarko93@gmail.com>
2022-12-05 20:52:37 +0200

    Changed Something

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
 console.log('Starting up');
+
+$(onInit)
+
+function onInit(){
+    renderProjects()
+}
+
+function renderProjects() {
+    var projects = getProjects()
+    var strHtmls = projects.map(project => `
+        <div class="col-md-6 col-sm-6 portfolio-item">
+          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+            <div class="portfolio-hover">
+              <div class="portfolio-hover-content">
+                <i class="fa fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img class="img-fluid" src="img/portfolio/${project.id}-thumbnail.jpg" alt="">
+          </a>
+          <div class="portfolio-caption">
+            <h4>${project.name}</h4>
+            <p class="text-muted">${project.title}</p>
+          </div>
+        </div>
+   `)
+   $('#photos').html(strHtmls)
+}
---
+++ js/proj-service.js
@@ -0,0 +1,45 @@
+'use strict'
+
+
+const gProjects = [
+    {
+     id: "book-store",
+     name: "Book store",
+     title: "Manage your book store effectively",
+     desc: "I was asked to build a dynamic web book store which presents the books, their price and description.",
+     url: "https://noahmarkovich.github.io/Book-shop-proj/",
+     publishedAt: 1672696800000,
+     labels: ["mvc", "keyboard events"],
+    },
+    {
+     id: "touch-the-nums",
+     name: "Touch the numbers",
+     title: "Hit those numbers as fast as you can",
+     desc: "A game I created in js vanila and basic css techniques at the early stages of the course",
+     url: "https://noahmarkovich.github.io/proj-touch-the-nums/",
+     publishedAt: 1671055200000,
+     labels: ["game", "keyboard events"],
+    },
+    {
+     id: "mine-sweeper",
+     name: "Minesweeper",
+     title: "Reveal the map without hitting the mines",
+     desc: "lorem ipsum lorem ipsum lorem ipsum",
+     url: "projs/sokoban",
+     publishedAt: 1671832800000,
+     labels: ["Matrixes", "keyboard events"],
+    },
+    {
+     id: "in-picture",
+     name: "In picture trivia game",
+     title: "Name the celeb in the picture",
+     desc: "lorem ipsum lorem ipsum lorem ipsum",
+     url: "projs/sokoban",
+     publishedAt: 1671051100000,
+     labels: ["Matrixes", "keyboard events"],
+    }
+    ]
+
+function getProjects() {
+    return gProjects
+}
